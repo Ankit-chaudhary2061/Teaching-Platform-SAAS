e9316550a49fee3651c3296df04cc6938fc8a9a8
Ankit Chaudhary <ankitchau2061@gmail.com>
2025-11-03 17:37:53 -0800

    create teacher controller

---
+++ src/Routes/institute/teacher/instituteTeacherRoutes.ts
@@ -0,0 +1,23 @@
+import express, { Router } from "express";
+import MiddleWare from "../../../middleWare/middleWare.ts";
+import TeacherController from "../../../controller/instititute/teacher/teacherController.ts";
+import { cloudinary,storage } from "../../../services/cloudinaryConfig.ts";
+import multer from "multer";
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 2 * 1024 * 1024, // 2 MB
+  },
+})
+
+const router: Router = express.Router();
+
+// Chain all table creations properly
+router.post(
+  '/teacher',
+ MiddleWare.isLogedIn,upload.single('teacherPhoto'),TeacherController.createTecaher
+);
+router.delete('/course/:id',MiddleWare.isLogedIn,TeacherController.createTecaher )
+router.get('/course',MiddleWare.isLogedIn,TeacherController.getTeachers )
+
+export default router;
---
+++ src/controller/instititute/teacher/teacherController.ts
@@ -0,0 +1,112 @@
+import sequelize from "../../../database/connection.ts";
+import { IExtendedRequest } from "../../../middleWare/type.ts";
+import { Response } from "express";
+import generateRandomPassword from "../../../services/generateRandomPassword.ts";
+import { QueryTypes } from "sequelize";
+
+class TeacherController{
+    static async createTecaher(req:IExtendedRequest, res :Response){
+        try {
+        const instituteNumber = req.user?.currentInstituteNumber;
+        if (!instituteNumber) {
+        return res.status(400).json({ message: "Institute number is missing" });
+        }
+        const {teacherName, teacherEmail, teacherJoinedDate, teacherExpertise, teacherSalary, teacherPhoneNumber, courseId} =req.body
+        if(!teacherName || ! teacherEmail || ! teacherJoinedDate || !teacherExpertise || !teacherSalary || ! teacherPhoneNumber){
+            return res.status(400).json({
+                message:'provide me teacherName, teacherEmail, teacherJoinedDate, teacherExpertise, teacherSalary, teacherPhoneNumber'
+            })
+
+        }
+
+        const password =generateRandomPassword(teacherName)
+        const teacherPhoto = req.file? req.file?.path : null
+        await sequelize.query(`INSERT INTO teacher_${instituteNumber}(teacherName, teacherEmail, teacherJoinedDate, teacherExpertise, teacherSalary, teacherPhoneNumber, teacherPhoto, teacherPassword) VALUES(?,?,?,?,?,?,?,?)`,{
+            replacements:[teacherName, teacherEmail, teacherJoinedDate, teacherExpertise, teacherSalary, teacherPhoneNumber, teacherPhoto, password.hashedVersion]
+        })
+        const teacherData: { id: string }[] = await sequelize.query(
+        `SELECT id FROM teacher_${instituteNumber} WHERE teacherEmail = ?`,
+        {
+          replacements: [teacherEmail],
+          type: QueryTypes.SELECT
+        })
+       
+        if (teacherData.length === 0) {
+        throw new Error("Teacher not found")
+         }
+
+        const teacherId = teacherData[0]?.id;
+        if(!teacherId){
+         return res.status(500).json({
+        message: "Teacher creation failed - unable to retrieve teacher ID",
+         });
+         }
+        await sequelize.query(
+          `UPDATE course_${instituteNumber} SET teacherId = ? WHERE Id = ?`,
+          {
+            type: QueryTypes.UPDATE,
+            replacements: [teacherId, courseId],
+        })
+        res.status(200).json({
+            message:'successfully create teacher '
+        })
+        } catch (error : any) {
+            console.error('Create teacher error:', error);
+      return res.status(500).json({
+        message: 'Server error',
+        error: error.message,
+        fullError: error,
+        stack: error.stack
+            
+        })}
+    }
+    static async getTeachers (req:IExtendedRequest, res:Response){
+        try {
+             const instituteNumber = req.user?.currentInstituteNumber;
+        if (!instituteNumber) {
+        return res.status(400).json({ message: "Institute number is missing" });
+        }
+        const teachers = await sequelize.query(`SELECT * FROM teacher_${instituteNumber}`,{
+            type:QueryTypes.SELECT
+        })
+
+        res.status(200).json({
+            message:'teachers fretch successfully ',
+            data : teachers
+        })
+        } catch (error : any) {
+                console.error('fetch teacher error:', error);
+      return res.status(500).json({
+        message: 'Server error',
+        error: error.message,
+        fullError: error,
+        stack: error.stack 
+        })
+    }
+}
+static async deleteTeacher (req:IExtendedRequest, res:Response){
+    try {
+           const instituteNumber = req.user?.currentInstituteNumber;
+        if (!instituteNumber) {
+        return res.status(400).json({ message: "Institute number is missing" });
+        }
+        const id  = req.params.id
+        await sequelize.query(`DELETE FROM teacher_${instituteNumber} WHERE id = ?`, {
+            type:QueryTypes.DELETE
+        } )
+        res.status(200).json({
+            message:'successfully delete  teacher'
+        })
+    } catch (error : any) {
+          console.error('delete teacher error:', error);
+      return res.status(500).json({
+        message: 'Server error',
+        error: error.message,
+        fullError: error,
+        stack: error.stack 
+        }) 
+    }
+}
+}
+
+export default TeacherController
